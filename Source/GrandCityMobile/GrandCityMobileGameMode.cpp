c85506641f739aa33afa3abaabfdad2ed9f652f6
Danny Wise <dannywise093@gmail.com>
2026-09-14 11:26:37 +0100

    Wire server-authoritative login and spawn flow

--- Source/GrandCityMobile/GrandCityMobileGameMode.cpp
+++ Source/GrandCityMobile/GrandCityMobileGameMode.cpp
@@ -1,11 +1,61 @@
 #include "GrandCityMobileGameMode.h"
 #include "GrandCityMobileCharacter.h"
-#include "GrandCityMobilePlayerState.h"
 #include "GrandCityMobileGameState.h"
+#include "GrandCityMobilePlayerController.h"
+#include "GrandCityMobilePlayerState.h"
+
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 AGrandCityMobileGameMode::AGrandCityMobileGameMode()
 {
     DefaultPawnClass = AGrandCityMobileCharacter::StaticClass();
+    PlayerControllerClass = AGrandCityMobilePlayerController::StaticClass();
     PlayerStateClass = AGrandCityMobilePlayerState::StaticClass();
     GameStateClass = AGrandCityMobileGameState::StaticClass();
+}
+
+void AGrandCityMobileGameMode::PostLogin(APlayerController* NewPlayer)
+{
+    Super::PostLogin(NewPlayer);
+
+    if (!NewPlayer)
+    {
+        return;
+    }
+
+    UpdateOnlinePlayerCount();
+}
+
+void AGrandCityMobileGameMode::Logout(AController* Exiting)
+{
+    Super::Logout(Exiting);
+
+    UpdateOnlinePlayerCount();
+}
+
+void AGrandCityMobileGameMode::RestartPlayer(AController* NewPlayer)
+{
+    if (!NewPlayer || !HasAuthority())
+    {
+        return;
+    }
+
+    Super::RestartPlayer(NewPlayer);
+}
+
+void AGrandCityMobileGameMode::UpdateOnlinePlayerCount()
+{
+    if (!HasAuthority() || !GetWorld())
+    {
+        return;
+    }
+
+    AGrandCityMobileGameState* CityGameState = GetGameState<AGrandCityMobileGameState>();
+    if (!CityGameState)
+    {
+        return;
+    }
+
+    CityGameState->OnlinePlayerCount = GetNumPlayers();
 }
