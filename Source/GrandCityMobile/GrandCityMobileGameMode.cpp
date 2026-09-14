31a46a7c08dcd6f35f8d22c91aefa8f9a247239c
Danny Wise <dannywise093@gmail.com>
2026-09-14 11:13:15 +0100

    Wire custom PlayerState and GameState into GameMode

--- Source/GrandCityMobile/GrandCityMobileGameMode.cpp
+++ Source/GrandCityMobile/GrandCityMobileGameMode.cpp
@@ -1,7 +1,11 @@
 #include "GrandCityMobileGameMode.h"
 #include "GrandCityMobileCharacter.h"
+#include "GrandCityMobilePlayerState.h"
+#include "GrandCityMobileGameState.h"
 
 AGrandCityMobileGameMode::AGrandCityMobileGameMode()
 {
     DefaultPawnClass = AGrandCityMobileCharacter::StaticClass();
+    PlayerStateClass = AGrandCityMobilePlayerState::StaticClass();
+    GameStateClass = AGrandCityMobileGameState::StaticClass();
 }
