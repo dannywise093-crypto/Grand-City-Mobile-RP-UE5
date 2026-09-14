b66ede642e95a722500252ae54b304727fdca3de
Danny Wise <dannywise093@gmail.com>
2026-09-14 10:53:24 +0100

    Add Grand City game mode

---
+++ Source/GrandCityMobile/GrandCityGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrandCityGameMode.generated.h"
+
+UCLASS()
+class GRANDCITYMOBILE_API AGrandCityGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AGrandCityGameMode();
+};
