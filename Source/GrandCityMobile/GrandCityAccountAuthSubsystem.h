3b5411aa6a30447a6bcfbf08f674c4a3020fb144
Danny Wise <dannywise093@gmail.com>
2026-09-14 11:50:07 +0100

    Add Unreal account auth subsystem foundation

---
+++ Source/GrandCityMobile/GrandCityAccountAuthSubsystem.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GrandCityAccountTypes.h"
+#include "GrandCityAccountAuthSubsystem.generated.h"
+
+delegate void FGrandCityAuthResult(bool bSuccess, const FGrandCityAccountIdentity& Identity);
+delegate void FGrandCitySessionResult(bool bSuccess, const FString& TransferToken);
+
+UCLASS()
+class GRANDCITYMOBILE_API UGrandCityAccountAuthSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    void VerifyToken(const FString& AuthToken, FGrandCityAuthResult Callback);
+    void ClaimSession(const FString& AccountId, const FString& ServerId, const FString& TransferToken, FGrandCitySessionResult Callback);
+    void ReleaseSession(const FString& AccountId, const FString& ServerId, TFunction<void(bool)> Callback);
+
+    bool IsAuthenticated() const { return Identity.bAuthenticated; }
+    const FGrandCityAccountIdentity& GetIdentity() const { return Identity; }
+
+private:
+    FString GetBaseUrl() const;
+    FString GetInternalApiKey() const;
+    void SetIdentity(const FGrandCityAccountIdentity& InIdentity);
+
+    FGrandCityAccountIdentity Identity;
+};
