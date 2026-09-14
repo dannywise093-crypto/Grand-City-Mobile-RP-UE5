f7846ae13263583f7e1a64a0d151aa8941b949c9
Danny Wise <dannywise093@gmail.com>
2026-09-14 11:11:44 +0100

    Add Grand City Mobile GameState foundation

---
+++ Source/GrandCityMobile/GrandCityMobileGameState.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GrandCityMobileGameState.generated.h"
+
+UCLASS()
+class GRANDCITYMOBILE_API AGrandCityMobileGameState : public AGameStateBase
+{
+    GENERATED_BODY()
+
+public:
+    AGrandCityMobileGameState();
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Grand City|World")
+    int32 OnlinePlayerCount = 0;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Grand City|World")
+    int32 ServerPopulationLimit = 100;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Grand City|World")
+    FName RegionId = TEXT("AFRICA_WEST");
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Grand City|World")
+    FName ServerId = TEXT("GC-AFRICA-01");
+
+protected:
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeReplicatedProps) const override;
+};
